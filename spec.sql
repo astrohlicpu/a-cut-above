dea9a00f3989ac891815fccf503d8c1c78939b5e
eanteby <eanteby2@gmail.com>
2024-08-05 20:31:03 -0400

    added spec

---
+++ spec.sql
@@ -0,0 +1,54 @@
+/*
+Hi, I'm Sara and I own a hair salon called 'A Cut Above'. Since its opening in 2016, my business has been growing and I've already hired 
+2 employees to help me service my many clients. It is becoming very difficult to keep track of appointments as well as 
+the money coming in. Therefore, I would like to have a database created that will record this information.
+
+I need to keep track of my clients' first names, last names, phone numbers, and the time of their appointments.
+I also need to record which services were scheduled/provided and how much money we received/are owed for each.
+
+I will need the following reports:
+1) upoming appointments- seperate report for each hairdresser, ordered by appointment time with more recent appointment 
+at the top of the list. Report should be formatted like this: Client first name client last name apt day of the week, apt datetime - service (price) phone number
+2) money owed- clients who already had their appointments and still haven't paid. report should show how many days late the payment is 
+3) best client- I need to know which client brought in the most money(including tips)
+4) report showing amount of money I made per month (not including tips)
+
+
+Q: Do clients need to pay at their appointments?
+A: I sometimes allow clients to pay at a later time. 
+
+Q: Do you ever recieve tips?
+A: yes!
+
+Q: Do you ever have more than one appointment at a time?
+A: yes, but not with the same hairdresser
+
+Q: Do you and your employees all work full time?
+A: No, I only come in at our busiest time: 3-5 pm 
+	Leah works full time: 10-7 
+	Penina works: 2-7
+I need to ensure that appointments are never accidently booked at times that we aren't in 
+
+Q: Are you open all days of the week
+A: No, only Sunday- Thursday
+
+Q: What services do you offer? 
+A: Haircuts for $25 and hairstyles for $36
+
+sample data in the following order: client first name, client last name, phone number, appointment time, day of the week, hairdresser, service, price, paid, tips
+Rivka, Adler, 845 425-5462, 7/2/24 10:15 am, Tues, Leah, haircut, $25, paid, $5
+Sara, Cohn, 914 582-0987, 7/4/24 5:45 pm, Thurs, Penina, hairstyle, $36, paid, $3
+Rochel, Weiss, 347 334-5466, 7/7/24 3:30 pm, Sun, Leah, hairstyle, $36, paid, $4
+Malka, Aron, 432 987-8776, 7/7/24 4:00 pm, Sun, Leah, haircut, $25, didn't pay
+Peri, Katz, 213 345-9876, 7/7/24 4:00 pm, Sun, Sara, haircut, $25, didn't pay
+Yali, Lax, 432 234-9864, 7/16/24, 11:30 am, Tues, Penina, hairstyle, $36, paid, $10
+Kaylee, Lax, 432 234-9864, 7/16/24, 12:00pm, Tues, Penina, hairstyle, $36, paid
+Faygie, Ackerman, 576 965-9090, 9/19/24 4:00 pm, thurs, sara, hairstyle, $36
+Esther, Bodner, 334 746-8665, 9/25/24 10:00 am, Wed, Penina, haircut, $25
+Dena, Pollack, 578 849-2002, 9/25/24 2:30 pm, Wed, Leah, hairstyle, $36
+
+
+
+
+*/
+
